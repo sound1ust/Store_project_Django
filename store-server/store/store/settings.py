16c6ad8643f38348edb19a5c1a682696488ad111
Pavel <pavelsoll2@gmail.com>
2023-02-07 22:23:40 +0300

    Installed django-extensions and ipython. Moved all products and categories to DB and import them to the project. Added fixtures.

--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 
     'products',
 ]
@@ -123,6 +124,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
         BASE_DIR / "static",
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
