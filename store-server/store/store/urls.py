07baf38a2e24abe2a1070e170ff9d34224da4e1d
Pavel <pavelsoll2@gmail.com>
2023-02-09 20:58:33 +0300

    Created login and registration pages and logic for login

--- store-server/store/store/urls.py
+++ store-server/store/store/urls.py
@@ -21,9 +21,10 @@ from django.conf import settings
 from products.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", index, name="index"),
     path("products/", include("products.urls", namespace="products")),
+    path("users/", include("users.urls", namespace="users"))
 ]
 
 if settings.DEBUG:
--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
-# Create your views here.
+from users.models import User
+from users.forms import UserLoginForm
+
+
+def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+
+    else:
+        form = UserLoginForm()
+    context = {"form": form}
+    return render(request=request, template_name='users/login.html', context=context)
+
+
+def registration(request):
+    return render(request=request, template_name='users/registration.html')
