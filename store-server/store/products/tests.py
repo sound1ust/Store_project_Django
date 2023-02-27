95eeff95bfdd50399d5ce8075caf26ffaf3b98f7
Pavel <pavelsoll2@gmail.com>
2023-02-27 22:41:00 +0300

    Added test for IndexView

--- store-server/store/products/tests.py
+++ store-server/store/products/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTestCase(TestCase):
+    def test_view(self):
+        path = reverse('index')  # http://127.0.0.1:8000/
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context_data['title'], 'Xstore')
+        self.assertTemplateUsed(response, 'products/index.html')
