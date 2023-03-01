ed5f62ec2031ab5913bffe6166cd2381f0f7949e
Pavel <pavelsoll2@gmail.com>
2023-03-01 22:11:22 +0300

    Added tests for ProductsList and UserRegistrationView

--- store-server/store/products/tests.py
+++ store-server/store/products/tests.py
@@ -1,5 +1,10 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 from django.urls import reverse
+
+from products.models import Product, ProductCategory
+from products.views import ProductsListView
 
 
 class IndexViewTestCase(TestCase):
@@ -7,6 +12,39 @@ class IndexViewTestCase(TestCase):
         path = reverse('index')  # http://127.0.0.1:8000/
         response = self.client.get(path)
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Xstore')
         self.assertTemplateUsed(response, 'products/index.html')
+
+
+class ProductsListViewTestCase(TestCase):
+    fixtures = ['categories.json', 'goods.json']
+
+    def _common_tests(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Каталог Xstore')
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def setUp(self):
+        self.products = Product.objects.all()
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        self._common_tests(response=response)
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products[:ProductsListView.paginate_by])
+        )
+
+    def test_list_with_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:category', kwargs={'category_id': category.id})
+        response = self.client.get(path)
+
+        self._common_tests(response=response)
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products.filter(category_id=category.id)[:ProductsListView.paginate_by])
+        )
--- store-server/store/users/tests.py
+++ store-server/store/users/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from users.forms import UserRegistrationForm
+
+
+class UserRegistrationViewTestCase(TestCase):
+    def setUp(self):
+        self.path = reverse('users:registration')
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Регистрация')
+        self.assertTemplateUsed(response, 'users/registration.html')
+
+    def test_user_registration_post_success(self):
+        data = {
+            'first_name': 'Pavel', 'last_name': 'Solenik',
+            'username': 'pavelsoll2', 'email': 'pavelsoll2@gmail.com',
+            'password1': '12345678qQ', 'password2': '12345678qQ',
+        }
+        response = self.client.post(self.path, data=data)
+
+
