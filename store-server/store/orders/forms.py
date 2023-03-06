9972be6f78f195f9a1ce5662b6379a68512d4cc5
Pavel <pavelsoll2@gmail.com>
2023-03-06 22:46:57 +0300

    Added an orders app, pages for it. Logic to create an order

---
+++ store-server/store/orders/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+from orders.models import Order
+
+
+class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введите имя получателя'
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введите фамилию получателя'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Адрес эл. почты получателя'
+    }))
+    address = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введите адрес получателя'
+    }))
+
+    class Meta:
+        model = Order
+        fields = ('first_name', 'last_name', 'email', 'address')
--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
 
     'products',
+    'orders',
     'users',
 ]
 
