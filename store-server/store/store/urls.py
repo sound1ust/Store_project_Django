46dc15fa487e10e8f5664d37b340e1ce7db7bd89
Pavel <pavelsoll2@gmail.com>
2023-04-08 23:37:43 +0300

    some changes for deploing project

--- store-server/store/store/urls.py
+++ store-server/store/store/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from products.views import IndexView
+
+static_urlpatterns = [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,6 +33,7 @@ urlpatterns = [
     path("users/", include("users.urls", namespace="users")),
     path('accounts/', include('allauth.urls')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path("", include(static_urlpatterns)),
 ]
 
 if settings.DEBUG:
